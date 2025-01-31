c683bba7157de665fbc1cbff45dc2d2385736a32
Kalpit Jare <79708651+kalpitjare118@users.noreply.github.com>
2025-01-31 14:36:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
